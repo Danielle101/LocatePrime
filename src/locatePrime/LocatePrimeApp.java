650d9cf7ee6b0f19299de4f4bfa12bbee1c2c921
Alex Marks <marks.alexanderj@gmail.com>
2016-04-22 16:04:41 -0400

    prints all primes until max

--- src/locatePrime/LocatePrimeApp.java
+++ src/locatePrime/LocatePrimeApp.java
@@ -3,12 +3,21 @@ import java.util.*;
 
 public class LocatePrimeApp {
 
-	public static void PrimeMethod(int input) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Let's located some primes!");
 		System.out.println("This application will find you any prime, in order, from first prime number on.\n");
 		System.out.println("Which prime number are you looking for?:");
+		int x = scan.nextInt();
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		
+			primes = PrimeMethod.allPrimes(x);
+			
+			for (int i = 0; i < primes.size(); i++) {
+				System.out.print(primes.get(i) + " ");
+			}
+			scan.close();
 	}
 
 }
--- src/locatePrime/PrimeMethod.java
+++ src/locatePrime/PrimeMethod.java
@@ -1,15 +1,26 @@
 package locatePrime;
 
+import java.util.ArrayList;
+
 public class PrimeMethod {
 	public static boolean isPrime(int number) {
-		boolean result = true;
-		for (int i = 2; i < number; i++) {
+		for (int i = 2; i < Math.sqrt(number); i++) {
 			if (number % i == 0) {
-				result = false;
+				return false;
 			}
 		}
 
-		return result;
+		return true;
+	}
+	
+	public static ArrayList<Integer> allPrimes(int max) {
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		for (int i = 2; i <= max; i++) {
+			if (isPrime(i)) {
+				primes.add(i);
+			}
+		}
+		return primes;
 	}
 
 }
